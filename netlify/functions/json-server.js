ecc4b9cc8b04df086aafb46e9e6748ece69f2cb6
mdrezuanislamridoy <mdrezuanislamridoy@gmail.com>
2024-09-04 20:26:20 +0600

    push

---
+++ netlify/functions/json-server.js
@@ -0,0 +1,22 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json"); // Path to your JSON file
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+
+module.exports.handler = async (event, context) => {
+  const result = await new Promise((resolve, reject) => {
+    server.listen(9000, () => {
+      resolve({
+        statusCode: 200,
+        body: JSON.stringify({
+          message: "JSON Server is running",
+        }),
+      });
+    });
+  });
+
+  return result;
+};
